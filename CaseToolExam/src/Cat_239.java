7c4b18c4afcb4ae885feb719f327a8a3c3b9383a
admin <admin@Bcalab3-m12>
2022-06-16 09:06:31 +0530

    Hello

---
+++ CaseToolExam/src/Cat_239.java
@@ -0,0 +1,15 @@
+
+public class Cat_239 {
+	String name;
+	
+	Cat(String name)
+	{
+		this.name=name;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
